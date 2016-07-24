268ede3af706c915e597ff712bb27ff1533750f7
Charles Kaminer <ckamine@g.clemson.edu>
2016-07-24 16:06:39 -0600

    complete python pangram challenge

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,4 +1,9 @@
 
 
-def is_pangram(string):
-    
+def is_pangram(sentence):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    clean = ''.join([i for i in sentence if i.isalpha()]).lower()
+    for char in alphabet:
+        if char not in clean:
+            return False
+    return True
