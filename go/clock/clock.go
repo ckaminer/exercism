7a919182a5f6e089fdb32d8ac836ac6646fca773
Charles Kaminer <ckamine@g.clemson.edu>
2017-07-07 10:24:22 -0400

    complete go clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,26 +1,33 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
 package clock
 
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
+import (
+  "fmt"
+)
+
 const testVersion = 4
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
-
-type Clock // Complete the type definition.  Pick a suitable data type.
+type Clock struct {
+    hour int
+    minute int
+}
 
 func New(hour, minute int) Clock {
+  returnMinute := minute % 60
+  if returnMinute < 0 {
+    returnMinute += 60
+    hour--
+  }
+  returnHour := (hour + minute/60) % 24
+  if returnHour < 0 {
+    returnHour += 24
+  }
+  return Clock{hour: returnHour, minute: returnMinute}
 }
 
-func (Clock) String() string {
+func (c *Clock) String() string {
+  return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
 }
 
-func (Clock) Add(minutes int) Clock {
+func (c Clock) Add(minutes int) Clock {
+  return New(c.hour, c.minute + minutes)
 }
-
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
